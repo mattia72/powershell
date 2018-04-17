e577f67b3f4457abd89c6cd6197a2e0d2036d066
mattia72 <mattiassicha@gmail.com>
2018-04-17 20:37:50 +0200

    update

--- Copy-Images.ps1
+++ Copy-Images.ps1
@@ -152,7 +152,7 @@ if (!(Test-Path -Path $To)) {
 
 Copy-Images -Source $From -Destination $To -NewExtension "jpg" -FilterWidth 1920 -Verbose
 
-Get-ItemsOlderThan -Days 30 -Path $To | Remove-Item 
+Get-ItemsOlderThan -Days 30 -Path $To | Remove-Item -Verbose 
 
 # Access denied :(
 #Get-ImageFile -path $To -FilterWidth 1080 | Select-Object Name,Width,Height
