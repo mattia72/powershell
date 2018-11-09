7a2ac68c4a73e4eac5cbaf49ab2b687a32c4064b
Mattiassich Akos - Agenda <mattiassich@agenda.de>
2018-11-09 10:34:02 +0100

    Add-XmlAttribute

--- Modify-Xml.ps1
+++ Modify-Xml.ps1
@@ -1,9 +1,28 @@
-#[xml]$xml = Get-Content "c:\msys64\home\mata\downloads\pelda_jav.xml"
-[xml]$xml = Get-Content C:\msys64\home\mata\downloads\export_zasob7.xml
-
-
-foreach ($name in $xml.responsePack.responsePackItem.listStock.stock.stockHeader.name)
+function Add-XMLAttribute([System.Xml.XmlNode] $node, [string] $attr_name, [string] $attr_value)
 {
-  $arr = $name.Split(";")
-  "<line1> $arr[0] </line1> <line2> $arr[1] </line2> "
+  $attrib = $node.OwnerDocument.CreateAttribute($attr_name)
+  $attrib.Value = $attr_value
+  $node.Attributes.Append($attrib)
+  $node.InnerText = ""
 }
+
+#[xml]$xml = Get-Content "c:\msys64\home\mata\downloads\pelda_jav.xml"
+$file = Get-Item "$env:USERPROFILE\Downloads\export_zasob7.xml"
+
+# load it into an XML object:
+$xml = New-Object -TypeName XML
+$xml.Load($file)
+
+$stk = @{stk = "http://www.stormware.cz/schema/version_2/stock.xsd"}
+$xnode = Select-Xml -Xml $xml -Namespace $stk -XPath "//stk:stockHeader/stk:name"
+#$xnode.GetType()
+
+foreach ($item in $xnode.Node)
+{
+  $arr = $item.InnerText.Split(";")
+  Add-XMLAttribute $item "line1" $arr[0].Trim()
+  Add-XMLAttribute $item "line2" $arr[1].Trim()
+  break
+}
+
+$xml.Save($(Join-Path $file.Directory "$($file.Basename)_saved.xml"))
