bffe1c2d2980dec12bd6e8c41bbc26530d15d4a6
Mattiassich Akos - Agenda <mattiassich@agenda.de>
2019-03-05 09:12:08 +0100

    Color table

---
+++ Get-ConsoleColors.ps1
@@ -0,0 +1,49 @@
+$fgColors = @(
+  "Black",
+  "DarkBlue",
+  "DarkGreen",
+  "DarkCyan",
+  "DarkRed",
+  "DarkMagenta",
+  "DarkYellow",
+  "Gray",
+  "DarkGray",
+  "Blue",
+  "Green",
+  "Cyan",
+  "Red",
+  "Magenta",
+  "Yellow",
+  "White"
+)
+$bgColors = @(
+  "Black",
+  "DarkBlue",
+  "DarkGreen",
+  "DarkCyan",
+  "DarkRed",
+  "DarkMagenta",
+  "DarkYellow",
+  "Gray",
+  "DarkGray",
+  "Blue",
+  "Green",
+  "Cyan",
+  "Red",
+  "Magenta",
+  "Yellow",
+  "White"
+)
+
+Write-Host $("{0,13} |" -f "Backgr/Foregr") -NoNewline
+foreach ($fgColor in $fgColors) {
+  Write-Host "$fgColor " -NoNewline
+}
+Write-Host "`n--------------------------------------------------------------------------------------------------------------------------------------"
+foreach ($bgColor in $bgColors) { 
+  Write-Host $("{0,13} |" -f $bgColor) -NoNewline
+  foreach ($fgColor in $fgColors) {
+    Write-Host "$fgColor " -ForegroundColor $fgColor -BackgroundColor $bgColor -NoNewline
+  }
+  Write-Host 
+}
