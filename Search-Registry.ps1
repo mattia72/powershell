3822263703f5bead9f027ab7ef279ab70572f5d8
mattia72 <mattiassicha@gmail.com>
2020-02-27 09:27:10 +0100

    Search-Registry

--- Search-Registry.ps1
+++ Search-Registry.ps1
@@ -1,3 +1,34 @@
+  [CmdletBinding()] 
+  param( 
+    [Parameter(Mandatory, Position = 0, ValueFromPipelineByPropertyName)] 
+    [Alias("PsPath")] 
+    # Registry path to search 
+    [string[]] $Path, 
+    # Specifies whether or not all subkeys should also be searched 
+    [switch] $Recurse, 
+    # A regular expression that will be checked against key names, value names, and value data (depending on the specified switches) 
+    [Parameter(ParameterSetName = "SingleSearchString", Mandatory)] 
+    [string] $SearchRegex, 
+# When the -SearchRegex parameter is used, this switch means that key names will be tested (if none of the three switches are used, keys will be tested) 
+    [Parameter(ParameterSetName = "SingleSearchString")] 
+    [switch] $KeyName, 
+# When the -SearchRegex parameter is used, this switch means that the value names will be tested (if none of the three switches are used, value names will be tested) 
+    [Parameter(ParameterSetName = "SingleSearchString")] 
+    [switch] $ValueName, 
+# When the -SearchRegex parameter is used, this switch means that the value data will be tested (if none of the three switches are used, value data will be tested) 
+    [Parameter(ParameterSetName = "SingleSearchString")] 
+    [switch] $ValueData, 
+# Specifies a regex that will be checked against key names only 
+    [Parameter(ParameterSetName = "MultipleSearchStrings")] 
+    [string] $KeyNameRegex, 
+    [Parameter(ParameterSetName = "MultipleSearchStrings")] 
+    # Specifies a regex that will be checked against value names only 
+    [string] $ValueNameRegex, 
+    [Parameter(ParameterSetName = "MultipleSearchStrings")] 
+    # Specifies a regex that will be checked against value data only 
+    [string] $ValueDataRegex 
+  ) 
+
 function Search-Registry { 
   <# 
 .SYNOPSIS 
@@ -47,7 +78,10 @@ Search-Registry -Path HKLM:\SOFTWARE\Microsoft -Recurse -ValueNameRegex "ValueNa
   begin { 
     switch ($PSCmdlet.ParameterSetName) { 
       SingleSearchString { 
-        $NoSwitchesSpecified = -not ($PSBoundParameters.ContainsKey("KeyName") -or $PSBoundParameters.ContainsKey("ValueName") -or $PSBoundParameters.ContainsKey("ValueData")) 
+        $NoSwitchesSpecified = -not ($PSBoundParameters.ContainsKey("KeyName") -or 
+          $PSBoundParameters.ContainsKey("ValueName") -or 
+          $PSBoundParameters.ContainsKey("ValueData")) -or 
+          (-not $KeyName -and -not $ValueName -and -not $ValueData)
         if ($KeyName -or $NoSwitchesSpecified) { $KeyNameRegex = $SearchRegex } 
         if ($ValueName -or $NoSwitchesSpecified) { $ValueNameRegex = $SearchRegex } 
         if ($ValueData -or $NoSwitchesSpecified) { $ValueDataRegex = $SearchRegex } 
@@ -56,53 +90,87 @@ Search-Registry -Path HKLM:\SOFTWARE\Microsoft -Recurse -ValueNameRegex "ValueNa
         # No extra work needed 
       } 
     } 
+    $GetChildItemError = ''
+    $CurrentLocation = Get-Location
   } 
 
   process { 
     foreach ($CurrentPath in $Path) { 
-      Get-ChildItem $CurrentPath -Recurse:$Recurse |  
-      ForEach-Object { 
-        $Key = $_ 
+      Push-Location $CurrentPath
+      Get-ChildItem . -Recurse:$Recurse -ErrorVariable +GetChildItemError |  
+        ForEach-Object { 
+          $Key = $_ 
 
-        if ($KeyNameRegex) {  
-          Write-Verbose ("{0}: Checking KeyNamesRegex" -f $Key.Name)  
+          if ($KeyNameRegex) {  
+            Write-Verbose ("{0}: Checking KeyNamesRegex" -f $Key.Name)  
 
-          if ($Key.PSChildName -match $KeyNameRegex) {  
-            Write-Verbose "  -> Match found!" 
+            if ($Key.PSChildName -match $KeyNameRegex) {  
+              Write-Verbose "$_  -> Match found!" 
+              return [PSCustomObject] @{ 
+                Key   = $Key | Resolve-Path -Relative
+                Value = $_
+                Reason = "KeyName" 
+              } 
+            }  
+          } 
+
+          if ($ValueNameRegex) {  
+            Write-Verbose ("{0}: Checking ValueNamesRegex" -f $Key.Name) 
+
+            if (($Key.GetValueNames() | ForEach-Object { 
+                $Name = $_
+                $Name
+                }) -match $ValueNameRegex) {  
+            Write-Verbose "$Name  -> Match found!" 
             return [PSCustomObject] @{ 
-              Key    = $Key 
-              Reason = "KeyName" 
-              Value = $Key.PSChildName
-            } 
-          }  
-        } 
+                Key   = $Key | Resolve-Path -Relative
+                Value = $Name
+                Reason = "ValueName" 
+              } 
+            }  
+          } 
 
-        if ($ValueNameRegex) {  
-          Write-Verbose ("{0}: Checking ValueNamesRegex" -f $Key.Name) 
+          if ($ValueDataRegex) {  
+            Write-Verbose ("{0}: Checking ValueDataRegex" -f $Key.Name) 
 
-          if ($Key.GetValueNames() -match $ValueNameRegex) {  
-            Write-Verbose "  -> Match found!" 
-            return [PSCustomObject] @{ 
-              Key    = $Key 
-              Reason = "ValueName" 
-              Value = $Key.GetValueNames
-            } 
-          }  
-        } 
-
-        if ($ValueDataRegex) {  
-          Write-Verbose ("{0}: Checking ValueDataRegex" -f $Key.Name) 
-
-          if (($Key.GetValueNames() | % { $Key.GetValue($_) }) -match $ValueDataRegex) {  
-            Write-Verbose "  -> Match!" 
-            return [PSCustomObject] @{ 
-              Key    = $Key 
-              Reason = "ValueData" 
-              Value = $Key.GetValue($_)
+            if (($Key.GetValueNames() | ForEach-Object { 
+                $Value = $Key.GetValue($_) 
+                $Value
+              }) -match $ValueDataRegex) {  
+              Write-Verbose "$Value  -> Match!" 
+              return [PSCustomObject] @{ 
+                Key   = $Key | Resolve-Path -Relative
+                Value = $Value
+                Reason = "ValueData" 
+              } 
             } 
           } 
         } 
-      } 
+      Pop-Location
     } 
+  } 
+  end{
+    Write-Host "$GetChildItemError"
+    Set-Location $CurrentLocation 
+  }
+} 
+
+switch ($PSCmdlet.ParameterSetName) { 
+  SingleSearchString { 
+    Search-Registry                     `
+      -Path $Path                     `
+      -Recurse:$Recurse               `
+      -SearchRegex $SearchRegex       `
+      -KeyName:$KeyName               `
+      -ValueName:$ValueName           `
+      -ValueData:$ValueData           `
+  } 
+  MultipleSearchStrings { 
+    Search-Registry                     `
+      -Path $Path                     `
+      -Recurse:$Recurse               `
+      -KeyNameRegex $KeyNameRegex     `
+      -ValueNameRegex $ValueNameRegex `
+      -ValueDataRegex $ValueDataRegex 
   } 
 } 
