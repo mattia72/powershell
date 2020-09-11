a3b7b07d514d4ac0b67b6612f51949a8caabd39e
Mattiassich Akos - Agenda <mattiassich@agenda.de>
2020-09-11 07:36:50 +0200

    XML filtered copy

--- Copy-FilteredXML.ps1
+++ Copy-FilteredXML.ps1
@@ -31,7 +31,98 @@ param (
   $ElementFilter
 ) 
 
-function Read-FilterAndWriteXml {
+function Read-AndWriteElementValueFilteredXml {
+  param (
+    [ValidateScript( {(Test-Path -Path $_) })]
+    $Src,
+    $Dest,
+    $Parent,
+    $Filter
+  )
+  
+  try 
+  {
+    # Set The Formatting
+    $xmlsettings = New-Object System.Xml.XmlWriterSettings
+    $xmlsettings.Indent = $true
+    $xmlsettings.IndentChars = " "
+    $SrcPath = $(Get-Item $Src).FullName
+    $DestPath = $(New-Item -path $Dest -ItemType File -Force).FullName
+    # Set the File Name Create The Document
+    $writer = [System.XML.XmlWriter]::Create($DestPath, $xmlsettings)
+    # $writer.WriteProcessingInstruction("xml-stylesheet", "type='text/xsl' href='style.xsl'")
+    $reader = [System.XML.XmlReader]::Create($SrcPath);
+    # $reader.WhitespaceHandling = WhitespaceHandling.None;
+    $parent_started = $false 
+    while ( $reader.Read()) {
+      # $dbg_msg = $reader.NodeType.ToString().PadRight(20, '-') 
+      # $dbg_msg += $reader.NodeType.ToString().PadRight(20, '-') 
+      # $dbg_msg +=  "> ".PadRight($reader.Depth*5) 
+      # $dbg_msg += $("Name = {0}, Value = {1}" -f  $reader.Name, $reader.Value)
+      # Write-Host $dbg_msg
+      switch ($reader.NodeType) {
+        'Element' { 
+          if ($parent_started) {
+            if ( $reader.Name -Match $Filter) {
+              $writer.WriteNode($reader, $false)
+            }
+          }
+          else {
+            $parent_started = ( $reader.Value -eq $Parent) 
+            if($reader.IsEmptyElement) {
+              $writer.WriteNode($reader, $false)
+            } else {
+              $writer.WriteStartElement($reader.Name)
+            }
+          }
+        }
+        'Text' {
+          if (-not $parent_started) {
+            $writer.WriteString($reader.Value)
+          }
+        }
+        'CDATA' {
+          if (-not $parent_started) {
+            $writer.WriteCData($reader.Value)
+          }
+        }
+        'ProcessingInstruction' {
+          $writer.WriteProcessingInstruction($reader.Name, $reader.Value)
+        }
+        'Comment' {
+          $writer.WriteNode($reader, $false)
+        }
+        'XmlDeclaration' {
+          $writer.WriteNode($reader, $false)
+        }
+        'DocumentType' {
+          $writer.WriteNode($reader, $false)
+        }
+        'EntityReference' {
+          $writer.WriteNode($reader, $false)
+        }
+        'EndElement' {
+          if ($parent_started -and $reader.Name -eq $Parent) {
+            $parent_started = $false
+          }  
+          if (-not $parent_started) {
+            $writer.WriteEndElement()
+          }
+        }
+      }
+    }
+  }
+  
+  catch {
+      Write-Error $_.Exception.Message
+  }
+  finally {
+    $writer.Flush()
+    $writer.Close()
+    $reader.Close()
+  }
+}
+function Read-AndWriteElementNameFilteredXml {
   param (
     [ValidateScript( {(Test-Path -Path $_) })]
     $Src,
@@ -123,4 +214,4 @@ function Read-FilterAndWriteXml {
   }
 }
 
-Read-FilterAndWriteXml -Src $XmlSrcPath -Dest $XmlDestPath -Filter $ElementFilter -Parent $ParentElement
+Read-AndWriteElementNameFilteredXml -Src $XmlSrcPath -Dest $XmlDestPath -Filter $ElementFilter -Parent $ParentElement
